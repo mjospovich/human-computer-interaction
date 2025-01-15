aa5bb59583357f5dcac7a551692f8aa49c3a337e
Luka <106626899+ldoric@users.noreply.github.com>
2025-01-15 19:00:47 +0100

    Update next.config.mjs

--- project/next.config.mjs
+++ project/next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   images: {
     remotePatterns: [
       {
@@ -15,6 +16,10 @@ const nextConfig = {
       use: ['@svgr/webpack'],
     });
     return config;
+  },
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
   },
 };
 
